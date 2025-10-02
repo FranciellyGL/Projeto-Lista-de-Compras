3c79b446c71cd5dac420e8d4a0918ba4cce3a511
FranciellyGL <82165308+FranciellyGL@users.noreply.github.com>
2025-10-01 23:47:09 -0300

    main.js

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-const compras= [];
-const tabela = document.getElementById("tabela");
-const form = document.getElementById("formlista");
-const inputNome = document.getElementById("nome");
-const limpar = document.getElementById("limpar");
-;
+const form = document.getElementById("formCadastro");
 
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-     const nome = inputNome.value.trim();
-    if(nome !== ""){
-        console.log("compras")
-        compras.push(nome)
-        atualizartabela()
-        form.reset() 
-        
-    }
+form.addEventListener("submit" , (evento) => {
+    evento.preventDefault()
+
+const nome = document.getElementById("nome").value
+const quantidade = document.getElementById("quantidade").value
+const preco = document.getElementById("preco").value
+const categoria = document.getElementById("categoria").value
+const origem = document.getElementById("origem").value
+const lote = document.getElementById("lote").value
+const validade = document.getElementById("validade").value
+
+let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+
+
+const novoProduto = {
+        nome: nome,
+        quantidade: quantidade,
+        preco: preco, 
+        categoria: categoria,
+        origem: origem,
+        lote: lote,
+        validade: validade
+    };
+     produtos.push(novoProduto);
+
+    localStorage.setItem("produtos", JSON.stringify(produtos))
+
+    alert("Produto Cadastrado!")
+    form.reset();
 })
-
-function atualizartabela(){
-    tabela.innerHTML = ""
-  compras.forEach((compras, index) => { 
-        const row = document.createElement("tr")
-        row.innerHTML = `<td> ${index + 1} </td> <td>${compras}</td>` 
-        tabela.appendChild(row)
-    })   
-}
-
-limpar.addEventListener("click", function() {
-        compras.length = 0; 
-        atualizartabela();
-});
